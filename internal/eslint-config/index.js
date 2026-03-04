25d53e701ba7bfff2b7357b086efc68ec5db2dbf
shuihang <2680027613@qq.com>
2026-03-04 20:01:17 +0800

    fix: 优化eslint代码规范

--- internal/eslint-config/index.js
+++ internal/eslint-config/index.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   env: {
     es6: true,
     browser: true,
-    node: true,
+    node: true
   },
   plugins: ['@typescript-eslint', 'prettier', 'unicorn'],
   extends: [
@@ -15,30 +15,30 @@ module.exports = defineConfig({
     'plugin:markdown/recommended',
     'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'prettier'
   ],
   settings: {
     'import/resolver': {
-      node: { extensions: ['.js', '.mjs', '.ts', '.d.ts', '.tsx'] },
-    },
+      node: { extensions: ['.js', '.mjs', '.ts', '.d.ts', '.tsx'] }
+    }
   },
   overrides: [
     {
       files: ['*.json', '*.json5', '*.jsonc'],
-      parser: 'jsonc-eslint-parser',
+      parser: 'jsonc-eslint-parser'
     },
     {
       files: ['*.ts', '*.vue'],
       rules: {
-        'no-undef': 'off',
-      },
+        'no-undef': 'off'
+      }
     },
     {
       files: ['**/__tests__/**'],
       rules: {
         'no-console': 'off',
-        'vue/one-component-per-file': 'off',
-      },
+        'vue/one-component-per-file': 'off'
+      }
     },
     {
       files: ['package.json'],
@@ -86,27 +86,27 @@ module.exports = defineConfig({
               'pnpm',
               'husky',
               'lint-staged',
-              'eslintConfig',
-            ],
+              'eslintConfig'
+            ]
           },
           {
             pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies$',
-            order: { type: 'asc' },
-          },
-        ],
-      },
+            order: { type: 'asc' }
+          }
+        ]
+      }
     },
     {
       files: ['*.d.ts'],
       rules: {
-        'import/no-duplicates': 'off',
-      },
+        'import/no-duplicates': 'off'
+      }
     },
     {
       files: ['*.js'],
       rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-      },
+        '@typescript-eslint/no-var-requires': 'off'
+      }
     },
     {
       files: ['*.vue'],
@@ -116,12 +116,12 @@ module.exports = defineConfig({
         extraFileExtensions: ['.vue'],
         ecmaVersion: 'latest',
         ecmaFeatures: {
-          jsx: true,
-        },
+          jsx: true
+        }
       },
       rules: {
-        'no-undef': 'off',
-      },
+        'no-undef': 'off'
+      }
     },
 
     {
@@ -129,9 +129,9 @@ module.exports = defineConfig({
       rules: {
         'no-console': 'off',
         'import/no-unresolved': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-      },
-    },
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    }
   ],
   rules: {
     // js/ts
@@ -143,19 +143,9 @@ module.exports = defineConfig({
     'no-return-await': 'error',
     'no-var': 'error',
     'no-empty': ['error', { allowEmptyCatch: true }],
-    'prefer-const': [
-      'warn',
-      { destructuring: 'all', ignoreReadBeforeAssign: true },
-    ],
-    'prefer-arrow-callback': [
-      'error',
-      { allowNamedFunctions: false, allowUnboundThis: true },
-    ],
-    'object-shorthand': [
-      'error',
-      'always',
-      { ignoreConstructors: false, avoidQuotes: true },
-    ],
+    'prefer-const': ['warn', { destructuring: 'all', ignoreReadBeforeAssign: true }],
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: false, allowUnboundThis: true }],
+    'object-shorthand': ['error', 'always', { ignoreConstructors: false, avoidQuotes: true }],
     'prefer-rest-params': 'error',
     'prefer-spread': 'error',
     'prefer-template': 'error',
@@ -179,8 +169,8 @@ module.exports = defineConfig({
         ignoreDeclarationSort: true,
         ignoreMemberSort: false,
         memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        allowSeparatedGroups: false,
-      },
+        allowSeparatedGroups: false
+      }
     ],
 
     // stylistic-issues
@@ -191,10 +181,7 @@ module.exports = defineConfig({
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      { disallowTypeAnnotations: false },
-    ],
+    '@typescript-eslint/consistent-type-imports': ['error', { disallowTypeAnnotations: false }],
     '@typescript-eslint/ban-ts-comment': ['off', { 'ts-ignore': false }],
 
     // vue
@@ -210,11 +197,11 @@ module.exports = defineConfig({
         html: {
           void: 'always',
           normal: 'always',
-          component: 'always',
+          component: 'always'
         },
         svg: 'always',
-        math: 'always',
-      },
+        math: 'always'
+      }
     ],
 
     // prettier
@@ -226,35 +213,26 @@ module.exports = defineConfig({
     'import/order': [
       'error',
       {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'object',
-          'type',
-        ],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
 
         pathGroups: [
           {
             pattern: 'vue',
             group: 'external',
-            position: 'before',
+            position: 'before'
           },
           {
             pattern: '@vue/**',
             group: 'external',
-            position: 'before',
+            position: 'before'
           },
           {
             pattern: '@cjx-low-code/**',
-            group: 'internal',
-          },
+            group: 'internal'
+          }
         ],
-        pathGroupsExcludedImportTypes: ['type'],
-      },
+        pathGroupsExcludedImportTypes: ['type']
+      }
     ],
     'import/no-unresolved': 'off',
     'import/namespace': 'off',
@@ -267,15 +245,15 @@ module.exports = defineConfig({
       {
         paths: [
           { name: 'lodash', message: 'Use lodash-unified instead.' },
-          { name: 'lodash-es', message: 'Use lodash-unified instead.' },
+          { name: 'lodash-es', message: 'Use lodash-unified instead.' }
         ],
         patterns: [
           {
             group: ['lodash/*', 'lodash-es/*'],
-            message: 'Use lodash-unified instead.',
-          },
-        ],
-      },
+            message: 'Use lodash-unified instead.'
+          }
+        ]
+      }
     ],
 
     // eslint-plugin-eslint-comments
@@ -318,6 +296,6 @@ module.exports = defineConfig({
     'unicorn/prefer-string-starts-ends-with': 'error',
     'unicorn/prefer-string-trim-start-end': 'error',
     'unicorn/prefer-type-error': 'error',
-    'unicorn/throw-new-error': 'error',
-  },
+    'unicorn/throw-new-error': 'error'
+  }
 })
